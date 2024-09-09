f64dfd0ea4d33aee19afc3d624e97e26732fb7b4
Ustaz07 <shamsutukor@gmail.com>
2024-09-09 11:54:53 +0000

    4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,44 +1,39 @@
 #!/usr/bin/env python3
-""" MRU caching system """
+"""Most Recently Used caching module.
+"""
+from collections import OrderedDict
 
 from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """ MRUCache class that inherits from BaseCaching
-        and implements an MRU caching system.
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with an MRU
+    removal mechanism when the limit is reached.
     """
-
     def __init__(self):
-        """ Initialize the class """
+        """Initializes the cache.
+        """
         super().__init__()
-        self.access_order = []  # List to track the order of key access
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """ Add an item in the cache """
+        """Adds an item in the cache.
+        """
         if key is None or item is None:
             return
-
-        # If the key is already in the cache, remove it to update the order later
-        if key in self.cache_data:
-            self.access_order.remove(key)
-
-        # Add the new item to cache and update access order
-        self.cache_data[key] = item
-        self.access_order.append(key)
-
-        # Check if cache exceeds MAX_ITEMS and discard the most recently used item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            mru_key = self.access_order.pop()  # The last element is the most recently used
-            del self.cache_data[mru_key]
-            print(f"DISCARD: {mru_key}")
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                mru_key, _ = self.cache_data.popitem(False)
+                print("DISCARD:", mru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an item by key """
-        if key is None or key not in self.cache_data:
-            return None
-
-        # Update the access order since this key was recently accessed
-        self.access_order.remove(key)
-        self.access_order.append(key)
-        return self.cache_data[key]
+        """Retrieves an item by key.
+        """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
