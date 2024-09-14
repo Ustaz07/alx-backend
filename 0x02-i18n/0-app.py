675983ba2c0ff3809e444903164ecc010e6415be
Ustaz07 <shamsutukor@gmail.com>
2024-09-14 05:35:03 +0000

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,10 +8,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     """Renders the index page with a title and a header"""
     return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
