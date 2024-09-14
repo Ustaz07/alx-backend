8b5782593b8a6ceacafe555aedaa64810969b2df
Ustaz07 <shamsutukor@gmail.com>
2024-09-14 05:33:47 +0000

    0-app.py

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app with a single route.
+Displays 'Welcome to Holberton' as the page title and 'Hello world' as the header.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Renders the index page with a title and a header"""
+    return render_template('0-index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
