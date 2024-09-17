0f59783f54009b7b014ff716d56f7e4653bf9889
Ustaz07 <shamsutukor@gmail.com>
2024-09-17 14:12:32 +0000

    4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,29 +1,48 @@
 #!/usr/bin/env python3
-from flask import Flask, request, render_template
+"""
+A Basic flask application
+"""
+from flask import Flask
+from flask import request
+from flask import render_template
 from flask_babel import Babel
 
-app = Flask(__name__)
 
-class Config:
+class Config(object):
+    """
+    Application configuration class
+    """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
+# Instantiate the application object
+app = Flask(__name__)
 app.config.from_object(Config)
+
+# Wrap the application with Babel
 babel = Babel(app)
 
+
 @babel.localeselector
-def get_locale():
-    # Check if the 'locale' parameter is in the URL query string
-    locale = request.args.get('locale')
-    if locale and locale in app.config['LANGUAGES']:
+def get_locale() -> str:
+    """
+    Gets locale from request object
+    """
+    locale = request.args.get('locale', '').strip()
+    if locale and locale in Config.LANGUAGES:
         return locale
-    # Fall back to the default behavior
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-@app.route('/')
-def index():
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Renders a basic html template
+    """
     return render_template('4-index.html')
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
